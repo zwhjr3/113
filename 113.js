28675c66e3903a5e03eb420e3a2464c27815e2e7
zwhjr3 <82552046+zwhjr3@users.noreply.github.com>
2024-03-09 18:55:19 -0600

    Add files via upload

---
+++ 113.js
@@ -0,0 +1,14 @@
+function setup() {
+    canvas = createCanvas(400, 400);
+    canvas.center();
+    video = createCapture(VIDEO);
+video.size(400, 400);
+video.hide();
+}
+
+function draw() {
+    circle(25 , 30 , 34)
+    rect(48, 58, 18, 38, 34)
+    rect(62, 12, 43, 41)
+    
+}
